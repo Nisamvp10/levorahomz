7226b66e070beab558ff673638fd2fbd8e1079fa
shikhil102 <shikhilshiki102@gmail.com>
2026-07-31 14:45:57 +0530

    pages updating

--- app/Views/frontend/contact-us.php
+++ app/Views/frontend/contact-us.php
@@ -1,140 +1,202 @@
 <?= view('frontend/inc/header') ?>
-  <!-- Page Title -->
-        <section class="section-page-title text-center flat-spacing-2 pb-0">
-            <div class="container">
-                <div class="main-page-title">
-                    <div class="breadcrumbs">
-                        <a href="index.html" class="text-caption-01 cl-text-3 link">Home</a>
-                        <i class="icon icon-CaretRightThin cl-text-3"></i>
-                        <P class="text-caption-01">
-                            Contact Us
-                        </P>
-                    </div>
-                    <h3>
-                        Contact Us
-                    </h3>
-                    <p class="text-body-1 cl-text-2">
-                        Get in touch with us for inquiries, support, or collaboration we’re here to help you.
-                    </p>
-                </div>
+<!-- Page Title -->
+<section class="section-page-title text-center pb-0 breadcrumbs-bg">
+    <div class="container">
+        <div class="main-page-title pb-40">
+            <div class="breadcrumbs pt-40">
+                <a href="index.html" class="text-caption-01 cl-text-3 link">Home</a>
+                <i class="icon icon-CaretRightThin cl-text-3"></i>
+                <P class="text-caption-01">
+                    Contact Us
+                </P>
             </div>
-        </section>
-        <!-- /Page Title -->
-        <!-- Map -->
-        <div class="section-map flat-spacing-2 pb-0">
-            <div class="container">
-                <div class="wg-map">
-                    <iframe
-                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7880.148272329334!2d151.20657421407668!3d-33.858885268389294!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6b12ae682c546039%3A0x16da940d587922a1!2sCircular%20Quay!5e0!3m2!1sen!2s!4v1745205798630!5m2!1sen!2s"
-                        allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-                </div>
-            </div>
+            <h3>
+                Let's talk
+            </h3>
+            <p class="text-body-1 cl-text-2">
+                Get in touch with us for inquiries, support, or collaboration we’re here to help you.
+            </p>
         </div>
-        <!-- /Map -->
-        <!-- Contact -->
-        <section class="section-contact flat-spacing">
-            <div class="container">
-                <div class="row gy-5 flex-wrap-reverse">
-                    <div class="col-md-6">
-                        <div class="col-left">
-                            <div class="heading d-grid gap-8">
-                                <h4>Information</h4>
-                                <h5 class="d-none">Perfect Heading SEO</h5>
-                                <p class="cl-text-2">
-                                    Have a question? Please contact us using the customer support channels below.
-                                </p>
-                            </div>
-                            <div class="grid-info tf-grid-layout sm-col-2">
-                                <div class="d-grid gap-8">
-                                    <h6>Phone:</h6>
-                                    <p>
-                                        <a href="tel:16662348888" class="cl-text-2 link">
-                                            +1 666 234 8888
-                                        </a>
-                                    </p>
-                                </div>
-                                <div class="d-grid gap-8">
-                                    <h6>Email:</h6>
-                                    <p>
-                                        <a href="mailto:hi.amere@gmail.com" class="cl-text-2 link">
-                                            hi.amere@gmail.com
-                                        </a>
-                                    </p>
-                                </div>
-                                <div class="wd-full d-grid gap-8">
-                                    <h6>Address:</h6>
-                                    <p>
-                                        <a href="https://www.google.com/maps?q=600+N+Michigan+Ave+Chicago,+IL+60611+USA"
-                                            target="_blank" class="cl-text-2 link">
-                                            2163 Phillips Gap Rd, West Jefferson, North Carolina, United States
-                                        </a>
-                                    </p>
-                                </div>
-                                <div class="wd-full d-grid gap-8">
-                                    <h6>Open Time:</h6>
-                                    <ul class="open-text">
-                                        <li class="d-flex gap-4 mb-4">
-                                            <span class="cl-text-2">Mon - Sat:</span>7:30am - 8:00pm PST
-                                        </li>
-                                        <li class="d-flex gap-4">
-                                            <span class="cl-text-2">Sunday:</span>9:00am - 5:00pm PST
-                                        </li>
-                                    </ul>
-                                </div>
+    </div>
+</section>
+<!-- /Page Title -->
+
+
+
+<!-- Map -->
+<div class="section-map pb-0">
+    <div class="container-fluid no-padding">
+        <div class="wg-map">
+            <iframe
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d62886.806394036415!2d76.67959106157846!3d9.898482841532847!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3b07c40a315b6705%3A0x4a8f3d2b78b03d27!2sThodupuzha%2C%20Kerala!5e0!3m2!1sen!2sin!4v1785488947875!5m2!1sen!2sin"
+                allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        </div>
+    </div>
+</div>
+<!-- /Map -->
+
+<!-- Contact -->
+
+<section class="showroom-location-container">
+    <div class="container">
+        <div class="col-md-12">
+            <div class="adventage-container">
+                <div class="row align-center">
+                    <div class="col-md-3 no-padding">
+                        <div class="box-icon_V01 wow fadeInLeft contact-location-item">
+                            <span class="icon">
+                                <i class="icon-Armchair"></i>
+                            </span>
+                            <div class="content">
+                                <h6 class="title">Thodupuzha</h6>
+                                <h3>+91 8113905557</h3>
+                                <p class="text cl-text-2"> Suite 305, Metro Plaza, Edappally, Kochi 682024, India</p>
                             </div>
                         </div>
                     </div>
-                    <div class="col-md-6">
-                        <h4 class="mb-8">Get In Touch</h4>
-                        <p class="mb-24 cl-text-2">
-                            Use the form below to get in touch with the sales team
-                        </p>
-                        <form class="form-get">
-                            <div class="form-content">
-                                <div class="tf-grid-layout sm-col-2">
-                                    <fieldset class="tf-field">
-                                        <label for="your-name" class="tf-lable fw-medium">
-                                            Your Name
-                                            <span class="text-primary">*</span>
-                                        </label>
-                                        <input type="text" id="your-name" placeholder="Your Name*" required>
-                                    </fieldset>
-                                    <fieldset class="tf-field">
-                                        <label for="your-email" class="tf-lable fw-medium">
-                                            Your Email
-                                            <span class="text-primary">*</span>
-                                        </label>
-                                        <input type="email" id="your-email" placeholder="Your Email*" required>
-                                    </fieldset>
-                                </div>
-                                <fieldset class="tf-field">
-                                    <label for="your-email" class="tf-lable fw-medium">
-                                        Your Message
-                                        <span class="text-primary">*</span>
-                                    </label>
-                                    <textarea placeholder="Your Message*" required></textarea>
-                                </fieldset>
-                                <div class="checkbox-wrap">
-                                    <input class="tf-check flex-shrink-0" type="checkbox" id="agree-term-2">
-                                    <label for="agree-term-2">
-                                        Save my name, email, and website in this browser for the next time I comment.
-                                    </label>
-                                </div>
+
+                    <div class="col-md-3 no-padding">
+                        <div class="box-icon_V01 wow fadeInLeft contact-location-item">
+                            <span class="icon">
+                                <i class="icon-Armchair"></i>
+                            </span>
+                            <div class="content">
+                                <h6 class="title">Trippunithura</h6>
+                                <h3>+91 8086017775</h3>
+                                <p class="text cl-text-2"> Suite 305, Metro Plaza, Edappally, Kochi 682024, India</p>
                             </div>
-                            <button type="submit" class="tf-btn animate-btn">
-                                Send message
-                            </button>
-                        </form>
+                        </div>
+                    </div>
+                    <div class="col-md-3 no-padding">
+                        <div class="box-icon_V01 wow fadeInLeft contact-location-item">
+                            <span class="icon">
+                                <i class="icon-Armchair"></i>
+                            </span>
+                            <div class="content">
+                                <h6 class="title">Koothattukulam</h6>
+                                <h3>+91 9539122022</h3>
+                                <p class="text cl-text-2"> Suite 305, Metro Plaza, Edappally, Kochi 682024, India</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-3 no-padding">
+                        <div class="box-icon_V01 wow fadeInLeft contact-location-item">
+                            <span class="icon">
+                                <i class="icon-Armchair"></i>
+                            </span>
+                            <div class="content">
+                                <h6 class="title">Mala</h6>
+                                <h3>+91 9048134377</h3>
+                                <p class="text cl-text-2"> Suite 305, Metro Plaza, Edappally, Kochi 682024, India</p>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
-        </section>
-        <!-- /Contact -->
+        </div>
+    </div>
+</section>
 
-    <!-- Page Contact Us End -->
+
+
+<section class="section-contact flat-spacing">
+    <div class="container contact-main-container">
+        <div class="row gy-5 flex-wrap-reverse">
+            <div class="col-md-6 no-margin padding-top-bottom">
+                <div class="col-left">
+                    <div class="heading d-grid gap-8">
+                        <h4>Get in touch with us</h4>
+                        <h5 class="d-none">Perfect Heading SEO</h5>
+                        <p class="cl-text-2">
+                            Have questions or need the right plywood or laminate for your project? Reach out to our team
+                            for reliable support.
+                        </p>
+                    </div>
+                    <div class="grid-info sm-col-2 contact-info-main-container">
+                        <div class="d-grid gap-8 contact-item">
+                            <h6>Email:</h6>
+                            <p>
+                                <a href="mailto:info@levorahome.com" class="cl-text-2 link">
+                                    info@levorahome.com
+                                </a>
+                            </p>
+                        </div>
+
+                        <div class="d-grid gap-8 contact-item">
+                            <h6>Phone:</h6>
+                            <p>
+                                <a href="tel:+918113905557" class="cl-text-2 link">
+                                    +91 8113905557
+                                </a>
+                            </p>
+                        </div>
+
+                        <div class="wd-full d-grid gap-8 contact-item">
+                            <h6>Address:</h6>
+                            <p>
+                                <a href="#" target="_blank" class="cl-text-2 link">
+                                    Suite 305, Metro Plaza, Edappally, Kochi 682024, India
+                                </a>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6 contact-form-container no-margin padding-top-bottom">
+                <!-- <h4 class="mb-8">Get In Touch</h4>
+                <p class="mb-24 cl-text-2">
+                    Use the form below to get in touch with the sales team
+                </p> -->
+                <form class="form-get">
+                    <div class="form-content">
+                        <div class="tf-grid-layout sm-col-2">
+                            <fieldset class="tf-field">
+                                <label for="your-name" class="tf-lable fw-medium">
+                                    Your Name
+                                    <span class="text-primary">*</span>
+                                </label>
+                                <input type="text" id="your-name" placeholder="Your Name*" required>
+                            </fieldset>
+                            <fieldset class="tf-field">
+                                <label for="your-email" class="tf-lable fw-medium">
+                                    Your Email
+                                    <span class="text-primary">*</span>
+                                </label>
+                                <input type="email" id="your-email" placeholder="Your Email*" required>
+                            </fieldset>
+                        </div>
+                        <fieldset class="tf-field">
+                            <label for="your-email" class="tf-lable fw-medium">
+                                Your Message
+                                <span class="text-primary">*</span>
+                            </label>
+                            <textarea placeholder="Your Message*" required></textarea>
+                        </fieldset>
+                        <div class="checkbox-wrap">
+                            <input class="tf-check flex-shrink-0" type="checkbox" id="agree-term-2">
+                            <label for="agree-term-2">
+                                Save my name, email, and website in this browser for the next time I comment.
+                            </label>
+                        </div>
+                    </div>
+                    <button type="submit" class="tf-btn animate-btn">
+                        Send message
+                    </button>
+                </form>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+
+
+<!-- /Contact -->
+
+<!-- Page Contact Us End -->
 <?= view('frontend/inc/footerLink') ?>
 
-    
+
 </body>
 
 </html>
